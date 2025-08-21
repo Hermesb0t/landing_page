a9f80268d325437fefe8551bafdbbb3d1143291e
bmon11 <bmon.2006@gmail.com>
2025-08-21 19:11:03 +0800

    footer changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,7 +38,7 @@ export default function Footer() {
           </div>
   
           <div className="mt-12 pt-8 border-t border-gray-800 flex flex-col md:flex-row justify-between items-center">
-            <p className="text-gray-400 text-sm mb-4 md:mb-0">© 2025 Тринитикс системс ХХК. All rights reserved.</p>
+            <p className="text-gray-400 text-sm mb-4 md:mb-0">© 2025 Trinityx Systems LLC. All rights reserved.</p>
             <div className="flex space-x-6">
             {/*
             <a href="#" className="text-gray-400 hover:text-white transition-colors">
